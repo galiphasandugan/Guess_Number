df442e685fe459c6aeedeff62b84a60fd7170985
galiphasandugan <galiphasandugan@gmail.com>
2023-05-26 00:58:04 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const buton = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const sonuc = document.querySelector(".sonuc");
+const par = document.querySelector("p");
+const random = Math.floor(Math.random() *100)
+console.log(random);
+
+let counter =7
+buton.addEventListener("click", () => {
+console.log(counter);
+console.log(input.value);
+
+ if(counter>0 && input.value>0 && input.value <100){
+  if (input.value > random) {
+    sonuc.value = "sayıyı azalt";
+    input.value = "";
+  }
+  if (input.value < random) {
+    sonuc.value = "sayıyı artır";
+    input.value = "";
+  }
+  if (input.value == random) {
+    sonuc.value = "KAZANDIN";
+  }
+  counter--
+ }else{
+    par.innerHTML= `HAKKINIZ KALMADI`
+    sonuc.value = "game over ";
+    return input.innerHTML = `input.value`;
+     
+ }
+  console.log(counter);
+  par.innerHTML= `${counter} HAKKINIZ KALDI`
+  input.focus();
+});
+
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    buton.click();
+  }
+});
+
+window.addEventListener("load", (e) => {
+  input.focus();
+});
